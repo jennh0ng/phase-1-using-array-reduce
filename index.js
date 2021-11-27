1a9acb3833daffb3625a0d8f92d556c3d4c237a2
Jennifer Hong <hongjenn24@gmail.com>
2021-11-27 00:08:00 -0800

    commit tree ccb2803fda4e85b77f79e54d82412440600f65e5

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// set up a variable called totalBatteries
+// use a reduce() method to loop through batteryBatches to return the sum of all elements
+
+// batteryAmount = element
+const totalBatteries = batteryBatches.reduce(function(accumulator, batteryAmount) {
+    return batteryAmount + accumulator}, 0);
+
+// arrow function format
+const totalBatteriesArrow = batteryBatches.reduce((accumulator, currentValue) => {
+    return currentValue + accumulator}, 0);
